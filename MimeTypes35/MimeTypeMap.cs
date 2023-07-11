4c22489443eedbbdee17503b7d8cd93b6fdb8ba6
Hudayfe Yurt <hudayfeyurt@hotmail.com>
2023-07-11 13:44:13 +0300

    init

---
+++ MimeTypes35/MimeTypeMap.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MimeTypes35
+{
+    public static class MimeTypeMap
+    {
+        private static readonly IDictionary<string, List<string>> Mappings;
+        private static readonly Dictionary<string, List<string>> SpecialMappings =
+            new Dictionary<string, List<string>>(StringComparer.OrdinalIgnoreCase)
+            {
+                {".mp3", new List<string>() {"audio/mpeg", "audio/mpeg3", "audio/x-mpeg-3"}},
+                {".zip", new List<string>() {"application/zip", "application/octet-stream", "application/x-zip-compressed"} }
+            };
+
+        static MimeTypeMap()
+        {
+            Mappings = new Dictionary<string, List<string>>(BuildMappings());
+        }
+
+        private static IDictionary<string, List<string>> BuildMappings()
+        {
+            var mapping = MimeMapping.Mappings;
+            foreach (var pair in SpecialMappings)
+            {
+                mapping[pair.Key] = pair.Value;
+            }
+            return mapping;
+        }
+
+        /// <summary>
+        /// Gets the MIME(s) by file extension.
+        /// </summary>
+        /// <param name="extension">The file extension</param>
+        /// <returns>List&lt;System.String&gt;Mime List.</returns>
+        /// <remarks>if there is no matching mimetype then will return "application/octet-stream"</remarks>
+        public static IEnumerable<string> GetMimeTypes(string extension)
+        {
+            if (string.IsNullOrEmpty(extension?.Trim()))
+            {
+                throw new ArgumentNullException(nameof(extension), "extension argument is null or empty.");
+            }
+
+            if (!extension.StartsWith("."))
+            {
+                extension = $".{extension}";
+            }
+
+            return Mappings.TryGetValue(extension, out List<string> mime) ? mime : new List<string>() { "application/octet-stream" };
+        }
+
+        /// <summary>
+        /// Gets the MIME by file extension.
+        /// </summary>
+        /// <param name="extension">The file extension</param>
+        /// <returns>List&lt;System.String&gt;Mime List.</returns>
+        /// <remarks>if there is no matching mimetype then will return "application/octet-stream"</remarks>
+        public static string GetMimeType(string extension)
+        {
+            if (string.IsNullOrEmpty(extension?.Trim()))
+            {
+                throw new ArgumentNullException(nameof(extension), "extension argument is null or empty.");
+            }
+
+            if (!extension.StartsWith("."))
+            {
+                extension = $".{extension}";
+            }
+
+            return Mappings.TryGetValue(extension, out List<string> mime) ? mime.First() : "application/octet-stream";
+        }
+
+        /// <summary>
+        /// Gets the extensions by mime type.
+        /// </summary>
+        /// <param name="mimeType">Type of the MIME</param>
+        /// <returns>IEnumerable&lt;System.String&gt; All known file extensions.</returns>
+        public static IEnumerable<string> GetExtensions(string mimeType)
+        {
+            if (string.IsNullOrEmpty(mimeType?.Trim()))
+            {
+                throw new ArgumentNullException(nameof(mimeType));
+            }
+
+            if (mimeType.StartsWith("."))
+            {
+                throw new ArgumentException($"Requested mime type is not valid: {mimeType}", nameof(mimeType));
+            }
+
+            return Mappings.Where(s => s.Value.Contains(mimeType, StringComparer.OrdinalIgnoreCase)).Select(s => s.Key);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# MimeTypes35
+
+Provides mapping of file extensions to mime types and mime types to file extensions.
+
+
+
+## Installation
+
+A NuGet package is available for easy integration into VisualStudio and automatic updates. Alternatively, you can clone and reference or copy the class to your project.
+
+```bash
+Install-Package MimeTypes35
+```
+
+## Support Platform
+
+Support .NET Framework 3.5 and above
+
+## Usage
+
+```cs
+using MimeTypes35;
+Console.WriteLine(MimeTypeMap.GetMimeType(".doc")); // "application/msword"
+Console.WriteLine(MimeTypeMap.GetMimeType("docx")); // "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+```
+
